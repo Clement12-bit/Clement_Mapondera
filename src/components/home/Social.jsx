2f3eaa106d8b370a27e699170f4e8b4995585bfd
Clement12-bit <clementmapondera09@gmail.com>
2024-10-08 20:02:03 +0200

    changes

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,16 +3,16 @@ import React from "react";
 const Social = () => {
     return(
         <div className="home__social">
-            <a href="#" className="home__social-icon" target="_blank">
+            <a href="https://m.me/clement.mapondera" className="home__social-icon" target="_blank">
                 <i class="uil uil-facebook-f"></i>
             </a>
 
-            <a href="#" className="home__social-icon" target="_blank">
+            <a href="https://wa.me/265993256552" className="home__social-icon" target="_blank">
                 <i class="uil uil-whatsapp"></i>
             </a>
 
-            <a href="#" className="home__social-icon" target="_blank">
-                <i class="uil uil-linkedin"></i>
+            <a href="mailto:clementmapondera09@gmail.com" className="home__social-icon" target="_blank">
+               <i class="uil uil-envelope"></i>
             </a>
         </div>
     )
