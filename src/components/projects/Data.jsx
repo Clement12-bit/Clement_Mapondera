40d9e29bda97c4be160708ce41877336f9373767
Clement12-bit <clementmapondera09@gmail.com>
2025-01-13 14:52:56 +0200

    changes

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,58 +1,58 @@
-import Work1 from "../../assets/travellers.png"
-import Work2 from "../../assets/hyms.png"
-import Work3 from "../../assets/work3.jpg"
-import Work4 from "../../assets/work4.jpg"
-import Work5 from "../../assets/work5.jpg"
+import Work1 from "../../assets/travellers.png";
+import Work2 from "../../assets/hyms.png";
+import Work3 from "../../assets/sms.png";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
 
 export const projectsData = [
-    {
-        id: 1,
-        image: Work1,
-        title: 'Web Design',
-        category: 'app',
-        url: 'https://malawitravellers.netlify.app',
-    },
-    {
-        id: 2,
-        image: Work2,
-        title: 'Mobile Apps',
-        category: 'web',
-        url: 'https://ccap-hyms.netlify.app/',
-    },
-    {
-        id: 3,
-        image: Work3,
-        title: 'Logos',
-        category: 'desings',
-        url: '/',
-    },
-    {
-        id: 4,
-        image: Work4,
-        title: 'Logos',
-        category: 'desings',
-        url: '/',
-    },
-    {
-        id: 5,
-        image: Work5,
-        title: 'Logos',
-        category: 'desings',
-        url: '/',
-    },
-]
+  {
+    id: 1,
+    image: Work1,
+    title: "Web Design",
+    category: "web",
+    url: "https://malawitravellers.netlify.app",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Mobile Apps",
+    category: "app",
+    url: "https://ccap-hyms.netlify.app/",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Web Design",
+    category: "web",
+    url: "https://school-system-jet.vercel.app",
+  },
+  {
+    id: 4,
+    image: Work4,
+    title: "Logos",
+    category: "desings",
+    url: "/",
+  },
+  {
+    id: 5,
+    image: Work5,
+    title: "Logos",
+    category: "desings",
+    url: "/",
+  },
+];
 
 export const projectsNav = [
-    {
-        name: 'all'
-    },
-    {
-        name: 'web'
-    },
-    {
-        name: 'app'
-    },
-    {
-        name: 'designs'
-    },
-]
+  {
+    name: "all",
+  },
+  {
+    name: "web",
+  },
+  {
+    name: "app",
+  },
+  {
+    name: "designs",
+  },
+];
